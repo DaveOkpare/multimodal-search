3edbe8abb447b27c5ffb3a06217f4a723b3b579a
David Okpare <okparedave@gmail.com>
2024-10-21 12:27:42 +0000

    Reformat code for readability and skip gallery posts in Reddit data extraction

--- fetch_reddit_posts.py
+++ fetch_reddit_posts.py
@@ -13,36 +13,53 @@ class Credential(BaseModel):
 def get_access_token(credentials: Credential, headers: dict) -> str:
     auth = requests.auth.HTTPBasicAuth(credentials.client_id, credentials.secret_key)
 
-    data = {'grant_type': 'password', 'username': credentials.username, 'password': credentials.password}
+    data = {
+        "grant_type": "password",
+        "username": credentials.username,
+        "password": credentials.password,
+    }
 
     # send our request for an OAuth token
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
+    res = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=auth,
+        data=data,
+        headers=headers,
+    )
 
     # convert response to JSON and pull access_token value
-    return res.json()['access_token']
+    return res.json()["access_token"]
 
 
-def fetch_data(subreddit: str, filter_by: Literal["hot", "new", "top"], headers: dict) -> list:
-    res = requests.get(f'https://oauth.reddit.com/r/{subreddit}/{filter_by}', headers=headers)
-    return res.json()['data']['children']
+def fetch_data(
+    subreddit: str, filter_by: Literal["hot", "new", "top"], headers: dict
+) -> list:
+    res = requests.get(
+        f"https://oauth.reddit.com/r/{subreddit}/{filter_by}", headers=headers
+    )
+    return res.json()["data"]["children"]
 
 
 def extract_reddit_post_info(posts: list):
     def build_output(data):
+        if "is_gallery" in data:
+            return None  # Skip gallery posts
+
         output = {
             "title": data["title"],
             "selftext": data["selftext"],
             "permalink": "https://reddit.com" + data["permalink"],
         }
-        
+
         if "post_hint" in data:
             output["image_url"] = data["url"]
-        elif "is_gallery" in data:
-            output["gallery_url"] = data["url"]
         else:
             output["url"] = data["url"]
 
         return output
 
-    return [build_output(post["data"]) for post in posts]
+    return [
+        post
+        for post in (build_output(post["data"]) for post in posts)
+        if post is not None
+    ]

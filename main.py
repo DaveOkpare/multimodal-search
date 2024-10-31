ef8ec4da8a84511d9214ff88e86e459406803ae6
David Okpare <okparedave@gmail.com>
2024-11-01 00:22:34 +0100

    Integrate Modal for automated Reddit processing with daily scheduled function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import modal
+
 from embed_ingest_utils import (
     embed_and_store_post,
     initialize_model,
@@ -13,6 +15,23 @@ from fetch_reddit_posts import (
 )
 
 load_dotenv()
+
+image = (
+    modal.Image.debian_slim(python_version="3.12")
+    .apt_install("git")
+    .pip_install([
+        "python-dotenv==1.0.1",
+        "requests==2.32.3",
+        "pydantic==2.9.2",
+        "transformers==4.45.2",
+        "timm==1.0.11",
+        "einops==0.8.0",
+        "qdrant_client==1.12.0",
+        "pillow==10.4.0",
+    ])
+)
+
+app = modal.App(image=image)
 
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_SECRET_KEY = os.getenv("REDDIT_SECRET_KEY")
@@ -36,16 +55,15 @@ headers = {**headers, **{"Authorization": f"bearer {token}"}}
 data = fetch_data(subreddit="pics", filter_by="hot", headers=headers)
 
 
+@app.function(schedule=modal.Period(days=1), secrets=[modal.Secret.from_name("multimodal-search")])
 def process_and_store_reddit_posts():
     model = initialize_model()
     client = setup_qdrant_client()
 
-    # [
-    #     embed_and_store_post(client, model, post)
-    #     for post in extract_reddit_post_info(data)
-    # ]
-
-    return model, client
+    [
+        embed_and_store_post(client, model, post)
+        for post in extract_reddit_post_info(data)
+    ]
 
 
 if __name__ == "__main__":
